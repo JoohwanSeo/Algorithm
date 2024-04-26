b05bfaa6f4408181b31047d5579f63dbd0ea2a62
Jony <104831702+JoohwanSeo@users.noreply.github.com>
2024-04-26 11:48:13 +0900

    Update 각도기.js

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,3 +1,19 @@
+//일반적인 if 조건문 사용
+function solution(angle) {
+ 
+  if (angle < 90) {
+    return 1;
+  } else if (angle == 90) {
+    return 2;
+  } else if (angle < 180) {
+    return 3;
+  } else {
+    return 4;
+  }
+}
+
+
+// 삼항 연산자 사용
 function solution(angle) {
   return angle < 90 ? 1 : angle == 90 ? 2 : angle < 180 ? 3 : 4;
 }
